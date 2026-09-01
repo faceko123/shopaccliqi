5220c06661d5a22b0dac74918bd9ac19263546cc
zakavo <facebook7.7.2007@gmail.com>
2026-09-01 09:59:57 +0700

    Update purchases.routes.js

--- backend/src/routes/purchases.routes.js
+++ backend/src/routes/purchases.routes.js
@@ -1,6 +1,6 @@
 const express = require("express");
 
-const db = require("../utils/db");
+const db = require("../utils/dbt");
 const { requireAuth } = require("../middleware/auth");
 
 const router = express.Router();
